e50213c782cc21a11f05ef01c359b23a63fb7050
Vishal Mistry <Vishal.Mistry@act.local>
2026-02-13 12:14:13 +0530

    Update code use function

--- Datefolders.py
+++ Datefolders.py
@@ -1,15 +1,14 @@
 import os
 from datetime import datetime, date, timedelta
 
-def datefolders():
-    c_date = datetime.today().date()
-    l_date = date(2026, 12, 31)
+c_date = datetime.today().date()
+l_date = date(2026, 12, 31)
 
-    if not os.path.exists("Date"):
-        os.mkdir("Date")
+
+def datefolders(c_date, l_date):
 
         for i in range((l_date-c_date).days+1):
             new_day = c_date + timedelta(days=i)
-            os.mkdir(f"Date/{new_day.strftime("%Y-%m-%d")}")
+            os.mkdir(f"C:/Users/vishal.mistry/Desktop/OS Module/{new_day.strftime("%Y-%m-%d")}")
 
-datefolders()
+datefolders(c_date,l_date)
