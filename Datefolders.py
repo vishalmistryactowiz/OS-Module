69a7ba92b95258d54dd6e0efdd33fadd340cfd0d
Vishal Mistry <Vishal.Mistry@act.local>
2026-02-13 14:40:33 +0530

    generate date wise folders from user input until year end using datetime and os module

--- Datefolders.py
+++ Datefolders.py
@@ -3,12 +3,12 @@ from datetime import datetime, date, timedelta
 
 user_date =input("Enter a Date YYYYMMDD:-")
 
-c_date = datetime.strptime(user_date,"%Y-%m-%d").date()
-
-l_date = date(c_date.year, 12, 31)
-
-def datefolders(c_date, l_date):
+def datefolders(user_date):
     try:
+        c_date = datetime.strptime(user_date, "%Y-%m-%d").date()
+        year = c_date.year
+        l_date = date(year, 12, 31)
+
         for i in range((l_date-c_date).days+1):
             new_day = c_date + timedelta(days=i)
             os.mkdir(f"C:/Users/vishal.mistry/Desktop/OS Module/{new_day.strftime("%Y-%m-%d")}")
@@ -18,4 +18,4 @@ def datefolders(c_date, l_date):
         print("Folders is already Created")
 
 
-datefolders(c_date,l_date)
+datefolders(user_date)
