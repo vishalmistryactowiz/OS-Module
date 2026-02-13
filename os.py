20e2eef8260a3eaae1645da5cc1646f2f01ce6c9
Vishal Mistry <Vishal.Mistry@act.local>
2026-02-13 10:42:43 +0530

    Second commit

--- os.py
+++ os.py
@@ -7,22 +7,12 @@ l_date = date(2026, 12, 31)
 
 remaining_days = (l_date - c_date).days
 
-main_folder = "Date"
-if not os.path.exists(main_folder):
-    os.mkdir(main_folder)
-
+if not os.path.exists("Date"):
+    os.mkdir("Date")
 
 if remaining_days > 0:
     for i in range(remaining_days + 1):
         new_day = c_date + timedelta(days=i)
-        folder_name = new_day.strftime("%Y-%m-%d")
+        os.mkdir(f"Date/{new_day.strftime("%Y-%m-%d")}")
 
-        # Create full path inside Date folder
-        folder_path = os.path.join(main_folder, folder_name)
 
-        if not os.path.exists(folder_path):
-            os.mkdir(folder_path)
-
-    print("Folders created successfully inside 'Date' directory!")
-else:
-    print("The date has already passed.")
